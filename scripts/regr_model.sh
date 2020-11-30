9a702eab49a52cb5c886ffb073de344c975db675
Michael Zhou <michaelzhou@Michaels-MacBook-Pro-2.local>
2020-11-30 16:19:23 -0500

    rm code snippet

--- scripts/regr_model.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#SBATCH --mem-per-cpu=10G -t 6:00:00 -p short
-#SBATCH --job-name=Modularity --output=slurm_out/100_edge_scores_wm/%j-True-999-1.01-edge_scores-wm-False.out
-python run_lin_regr_w_feature_sel.py 999 1.01 edge_scores wm True False
